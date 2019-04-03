4ea55ad9fadeab28dc495d6990f854a7d0c56444
Nathan Worden <worden.nathan@gmail.com>
2019-04-03 06:55:35 -0500

    Part 1 Study Guide for Test

---
+++ 02.Part 1 Study Guide for Test.rb
@@ -0,0 +1,30 @@
+# Part 1: Study Guide for Test
+
+# Assessment RB 109 will test you knowledge of courses LS 100 and RB101, which
+# has a huge surface area in that it covers the RUby programming language
+# broadly. It will not cover Object Oriented Programming.
+
+# Specific Topics of Interest
+
+# Be able to explain clearly the following topics:
+
+# Local variable scope, especially how local variables interact with
+# method invocations with bocks blocks and method definitions.
+
+# How passing an object into a method definition can or cannot permanently
+# change the object.
+
+# Working with collections (Array, Hash, Sting), and popular collection 
+# methods (each, map, select, etc). Review the two lessons on these topics
+# thoroughly.
+
+# variables as pointers
+
+# puts vs. return
+
+# false vs nil and the idea of "Truthiness"
+
+# Method definition and method invocation
+
+# Implicit Return Value of Method Invocations and blocks
+
