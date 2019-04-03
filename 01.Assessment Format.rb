53898b8793bb542437dc8e6a7877579e75f96d35
Nathan Worden <worden.nathan@gmail.com>
2019-04-03 06:50:14 -0500

    Assessment Format

---
+++ 01.Assessment Format.rb
@@ -0,0 +1,22 @@
+Assessment Format
+This assessment will be done in 2 parts:
+
+A timed test that you can start whenever you are ready
+A 1-on-1 live coding interview using Slack Calls or Skype
+Not passing either part results in a "Not Yet" for this assessment. See Assessment Rules for the policy on retakes.
+
+Part 1 - Test
+The test constitutes part 1 of this assessment. It will comprise of a series of questions (around 20) and you will have 3 hours to complete it. After you submit the test, it will be graded within 24 hours. After the initial grading, you might be asked to revise your answers, even if you receive a high grade. After you submit acceptable revisions, the grader will then submit the test to the instructor for a final evaluation. The instructor will take a look at your initial answers and revisions, and determine if the work is sufficient to pass. The instructor will make a final evaluation within 24 hours of receiving the test from the grader. The entire process could take a few days, depending on how well you do on the initial answers and how quickly you respond to requests for revisions.
+
+If you pass, you can continue on to part 2. If you do not pass, you will need to retake this test again according to the retake policy.
+
+There will be a study guide for the test that walks you through exactly what to study for and how to prepare.
+
+Part 2 - Interview
+Once you pass part 1, you'll be sent instructions on scheduling a follow-up interview.
+
+The interview will be conducted very similarly to a real job interview, except we'll only be talking about the topics covered in RB101 Programming Foundations. Make sure to be clear on the core concepts from the lessons in that course, and be able to explain clearly with code examples during the interview. You'll be expected to explain with precision and clarity, while at the same time doing so with code examples.
+
+We'll conduct the interview using Slack Calls or Skype, along a collaborative online editor. We'll primarily use Coderpad and consider Cloud 9 as an alternative. You should practice using the Coderpad environment ahead of time by exploring the demo pad. This pad is specifically set up for demonstration purposes only and will not be used for the actual interview. When you're done exploring the demo pad, make sure to clear any changes you made so that it's ready for the next person to use.
+
+There will be a study guide for the interview that walks you through exactly what to study for and how to prepare.
