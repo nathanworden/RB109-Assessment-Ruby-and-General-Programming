566a738c42770835216747aa20cab1cb34223ad9
Nathan Worden <worden.nathan@gmail.com>
2019-04-09 18:11:24 -0500

    studyinig for test

--- 02.part1_beginning_ruby.rb
+++ 02.part1_beginning_ruby.rb
@@ -1,2 +1,13 @@
-puts("hello") # This is a method invocation
+# def amethod(param)
+# param += " universe"
+# # param + " universe"
+# param << " world"
+# end
 
+# str = "hello"
+
+# amethod(str)
+
+# p str
+
+a = "hello"
