9457fa9b2517cd1d5802908f82119f47b235bbba
Nathan Worden <worden.nathan@gmail.com>
2019-04-10 07:00:36 -0500

    studying for test

---
+++ 02.my_study_guide_practice_questions.rb
@@ -0,0 +1,25 @@
+#1) What will this code return?
+def amethod(param)
+  param + " Lee"
+end
+
+str = "Chris"
+amethod(str)
+p str
+# Answer: 
+# => "Chris"
+
+#2) What will this code return?
+def amethod(param)
+  param << " Lee"
+end
+
+str = "Chris"
+p amethod(str)
+# Answer:
+# => "Chris Lee"
+
+#3) What will this code return?
+def amethod(param)
+  
+end
--- 02.part1_beginning_ruby.rb
+++ 02.part1_beginning_ruby.rb
@@ -1,13 +1,1 @@
-# def amethod(param)
-# param += " universe"
-# # param + " universe"
-# param << " world"
-# end
-
-# str = "hello"
-
-# amethod(str)
-
-# p str
-
-a = "hello"
+# Notes
