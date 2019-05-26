3983004c4eb1bf0173d24183230305c51dc726a4
Nathan Worden <worden.nathan@gmail.com>
2019-05-25 23:14:43 -0500

    study

--- 05.Part2 Study Guide for Interview/17.temp_computer.rb
+++ 05.Part2 Study Guide for Interview/17.temp_computer.rb
@@ -1,15 +1,20 @@
+
+
 # Create a pemdas array
 # Loop through the numbers array
 # If an element is a :* or a :/ symbol
     # initiate a 'start' variable to the current index
     # initiate a 'finish' variable to the current index
     # 'start' = current index - 1
+    # 
 
 require 'pry'
 
 NUMBERS = {'zero' => 0, 'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4,  'five' => 5, 'six' => 6, 
             'seven' => 7, 'eight' => 8, 'nine' => 9, 'ten' => 10, 'plus' => :+, 'minus' => :-, 
             'divided' => :/, 'times' => :*}
+
+
 
 def operate!(order)
   loop do
@@ -18,6 +23,29 @@ def operate!(order)
     break if order.size == 1
   end
 end
+
+
+def find_start_and_end_of_multiplication_and_division(english)
+  current_index = 1
+  start = current_index - 1
+  final = current_index + 1
+  loop do
+    if english[current_index] == :* || english[current_index] == :/
+      start = current_index - 1
+      until english[current_index] == :+ || english[current_index] == :- || current_index >= english.size - 1
+        final = current_index + 1
+        current_index += 2
+      end
+      break
+    else
+      break if english.length - 1 == current_index 
+      current_index += 2
+    end
+  end
+  [start, final]
+end
+
+
 
 def computer(string)
   english = string.split(" ")
@@ -28,22 +56,11 @@ def computer(string)
 
   while english.any? {|element| element == :* || element == :/}
     break if !english.include?(:+) && !english.include?(:-)
-    current_index = 1
-    start = current_index - 1
-    final = current_index + 1
-    loop do
-      if english[current_index] == :* || english[current_index] == :/
-        start = current_index - 1
-        until english[current_index] == :+ || english[current_index] == :- || current_index >= english.size - 1
-          final = current_index + 1
-          current_index += 2
-        end
-        break
-      else
-        break if english.length - 1 == current_index 
-        current_index += 2
-      end
-    end
+
+
+    start, final = find_start_and_end_of_multiplication_and_division(english)
+
+
     multiplication_division = english[start, (final - start + 1)]
     operate!(multiplication_division)
     english = [english[0, start], multiplication_division, english[final + 1, english.size]].flatten
